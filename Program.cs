63bd4aba2b750ac7c57d7e005ab3acf2325598e9
john <john@primitive.io>
2024-04-26 14:39:49 -0700

    download step files

--- Program.cs
+++ Program.cs
@@ -2,25 +2,41 @@ using System.Collections.Specialized;
 using Google.Apis.YouTube.v3;
 using Google.Apis.Services;
 using Google.Apis.YouTube.v3.Data;
+using HtmlAgilityPack;
 using Newtonsoft.Json;
 
 namespace StepScraper;
 
 public class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
         string pdfFolderPath = args[0];
 
         List<string> youtubeLinks = DocReader.ReadFolderAndExtractYoutubeLinks(pdfFolderPath);
         List<string> videoIds = youtubeLinks.Select(ExtractVideoIdFromUrl).ToList();
-        
+
         string jsonPath = Path.Combine(pdfFolderPath, "videoTitleAndDescriptions.json");
-        string jsonFileText = File.ReadAllText(jsonPath);
+        string jsonFileText = await File.ReadAllTextAsync(jsonPath);
         Dictionary<string, Tuple<string, string>> videoTitleAndDescriptions =
             JsonConvert.DeserializeObject<Dictionary<string, Tuple<string, string>>>(jsonFileText) ?? [];
 
-        ScrapeYoutubeAndWriteJson(videoIds, videoTitleAndDescriptions, args[1], jsonPath);
+        //ScrapeYoutubeAndWriteJson(videoIds, videoTitleAndDescriptions, args[1], jsonPath);
+
+        List<string> stepZipUrls = [];
+        foreach ((string? title, string? description) in videoTitleAndDescriptions.Values)
+        {
+            string[] lines = description.Split('\n');
+            foreach (string line in lines)
+            {
+                if (line.EndsWith("STEP.zip"))
+                {
+                    stepZipUrls.Add(line);
+                }
+            }
+        }
+
+        await DownloadStepFiles(stepZipUrls, pdfFolderPath);
     }
 
     static string ExtractVideoIdFromUrl(string url)
@@ -31,14 +47,14 @@ public class Program
     }
 
     static void ScrapeYoutubeAndWriteJson(
-        List<string> videoIds, 
+        List<string> videoIds,
         IDictionary<string, Tuple<string, string>> videoTitleAndDescriptions,
         string apiKey,
         string jsonPath)
     {
         const int max = 10;
         int count = 0;
-        
+
         foreach (string videoId in videoIds)
         {
             if (videoTitleAndDescriptions.ContainsKey(videoId)) continue;
@@ -78,5 +94,63 @@ public class Program
 
         File.WriteAllText(jsonPath, json);
         Console.WriteLine("json written to " + jsonPath);
+    }
+
+    static async Task DownloadStepFiles(List<string> stepZipUrls, string pdfFolderPath)
+    {
+        foreach (string stepZipUrl in stepZipUrls)
+        {
+            string downloadUrl = await FetchDownloadLink(stepZipUrl);
+            if (string.IsNullOrEmpty(downloadUrl)) continue;
+
+            // download zip file
+            string zipPath = Path.Combine(pdfFolderPath, stepZipUrl[(stepZipUrl.LastIndexOf('/') + 1)..]);
+            Console.WriteLine($"Downloading {stepZipUrl} to {zipPath}");
+            using HttpClient client = new HttpClient();
+
+            try
+            {
+                byte[] bytes = await client.GetByteArrayAsync(downloadUrl);
+                await File.WriteAllBytesAsync(zipPath, bytes);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+
+    static async Task<string> FetchDownloadLink(string url)
+    {
+        using HttpClient httpClient = new HttpClient();
+        HttpResponseMessage response = await httpClient.GetAsync(url);
+        try
+        {
+            response.EnsureSuccessStatusCode();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return "";
+        }
+
+        string htmlContent = await response.Content.ReadAsStringAsync();
+
+        HtmlDocument htmlDoc = new HtmlDocument();
+        htmlDoc.LoadHtml(htmlContent);
+
+        HtmlNode? downloadButton = htmlDoc.DocumentNode.SelectSingleNode("//*[@id='downloadButton']");
+        if (downloadButton == null)
+        {
+            throw new InvalidOperationException("Download button not found.");
+        }
+
+        string? downloadLink = downloadButton.GetAttributeValue("href", string.Empty);
+        if (string.IsNullOrEmpty(downloadLink))
+        {
+            throw new InvalidOperationException("No download link found in the download button.");
+        }
+
+        return downloadLink;
     }
 }

a9fdd51f1e9d08a990a226deedd03aa3d2a3a4da
john <john@primitive.io>
2024-05-03 19:08:43 -0700

    more info

--- Program.cs
+++ Program.cs
@@ -9,6 +9,24 @@ namespace StepScraper;
 
 public class Program
 {
+    public enum FileData
+    {
+        DEAD = 0,
+        DESCRIPTION = 1,
+        STEP = 2,
+        INV = 3,
+        STEP_INV = 4
+    }
+
+    public class VideoAndLinks
+    {
+        public string Title;
+        public string Description;
+        public string StepLink;
+        public string InvLink;
+        public FileData FileData;
+    }
+
     public static async Task Main(string[] args)
     {
         string pdfFolderPath = args[0];
@@ -18,33 +36,41 @@ public class Program
 
         string jsonPath = Path.Combine(pdfFolderPath, "videoTitleAndDescriptions.json");
 
-        Dictionary<string, Tuple<string, string>> videoTitleAndDescriptions = new();
+        Dictionary<string, VideoAndLinks> videoTitleAndDescriptions = new();
         if (!File.Exists(jsonPath))
         {
             string json = JsonConvert.SerializeObject(videoTitleAndDescriptions, Formatting.Indented);
             await File.WriteAllTextAsync(jsonPath, json);
         }
-        
+
         string jsonFileText = await File.ReadAllTextAsync(jsonPath);
         videoTitleAndDescriptions =
-            JsonConvert.DeserializeObject<Dictionary<string, Tuple<string, string>>>(jsonFileText) ?? [];
+            JsonConvert.DeserializeObject<Dictionary<string, VideoAndLinks>>(jsonFileText) ?? [];
 
-        ScrapeYoutubeAndWriteJson(videoIds, videoTitleAndDescriptions, args[1], jsonPath);
+        ScrapeYoutube(videoIds, videoTitleAndDescriptions, args[1]);
 
-        List<string> stepZipUrls = [];
-        foreach ((string? title, string? description) in videoTitleAndDescriptions.Values)
+        foreach (VideoAndLinks videoAndLinks in videoTitleAndDescriptions.Values)
         {
-            string[] lines = description.Split('\n');
+            string[] lines = videoAndLinks.Description.Split('\n');
             foreach (string line in lines)
             {
                 if (line.EndsWith("STEP.zip"))
                 {
-                    stepZipUrls.Add(line);
+                    videoAndLinks.StepLink = line;
+                }
+                else if (line.EndsWith("Inv.zip"))
+                {
+                    videoAndLinks.InvLink = line;
                 }
             }
         }
 
-        await DownloadStepFiles(stepZipUrls, pdfFolderPath);
+        await DownloadStepFiles(videoTitleAndDescriptions.Values.ToList(), pdfFolderPath);
+        
+        string jsonFinal = JsonConvert.SerializeObject(videoTitleAndDescriptions, Formatting.Indented);
+
+        await File.WriteAllTextAsync(jsonPath, jsonFinal);
+        Console.WriteLine("json written to " + jsonPath);
     }
 
     static string ExtractVideoIdFromUrl(string url)
@@ -54,11 +80,10 @@ public class Program
         return query["v"] ?? url[(url.LastIndexOf('/') + 1)..];
     }
 
-    static void ScrapeYoutubeAndWriteJson(
+    static void ScrapeYoutube(
         List<string> videoIds,
-        IDictionary<string, Tuple<string, string>> videoTitleAndDescriptions,
-        string apiKey,
-        string jsonPath)
+        Dictionary<string, VideoAndLinks> videoTitleAndDescriptions,
+        string apiKey)
     {
         const int max = 10;
         int count = 0;
@@ -92,40 +117,76 @@ public class Program
                 Video? video = response.Items[0];
                 videoTitleAndDescriptions.Add(
                     videoId,
-                    new Tuple<string, string>(video.Snippet.Title, video.Snippet.Description));
+                    new VideoAndLinks
+                    {
+                        Title = video.Snippet.Title,
+                        Description = video.Snippet.Description,
+                        FileData = FileData.DESCRIPTION
+                    });
             }
 
             count++;
         }
 
-        string json = JsonConvert.SerializeObject(videoTitleAndDescriptions, Formatting.Indented);
-
-        File.WriteAllText(jsonPath, json);
-        Console.WriteLine("json written to " + jsonPath);
     }
 
-    static async Task DownloadStepFiles(List<string> stepZipUrls, string pdfFolderPath)
+    static async Task DownloadStepFiles(List<VideoAndLinks> videoAndLinksList, string pdfFolderPath)
     {
-        foreach (string stepZipUrl in stepZipUrls)
+        foreach (VideoAndLinks videoAndLinks in videoAndLinksList)
         {
-            string downloadUrl = await FetchDownloadLink(stepZipUrl);
-            if (string.IsNullOrEmpty(downloadUrl)) continue;
-
-            // download zip file
-            string zipPath = Path.Combine(pdfFolderPath, stepZipUrl[(stepZipUrl.LastIndexOf('/') + 1)..]);
-            Console.WriteLine($"Downloading {stepZipUrl} to {zipPath}");
-            using HttpClient client = new();
-
-            try
+            bool stepSuccess = false;
+            bool invSuccess = false;
+            if (!string.IsNullOrEmpty(videoAndLinks.StepLink))
             {
-                byte[] bytes = await client.GetByteArrayAsync(downloadUrl);
-                await File.WriteAllBytesAsync(zipPath, bytes);
+                stepSuccess  = await X(videoAndLinks.StepLink, pdfFolderPath);
             }
-            catch (Exception e)
+
+            if (!string.IsNullOrEmpty(videoAndLinks.InvLink))
             {
-                Console.WriteLine(e.Message);
+                invSuccess = await X(videoAndLinks.InvLink, pdfFolderPath);
+            }
+
+            if (stepSuccess && invSuccess)
+            {
+                videoAndLinks.FileData = FileData.STEP_INV;
+            }
+            else if (stepSuccess)
+            {
+                videoAndLinks.FileData = FileData.STEP;
+            }
+            else if (invSuccess)
+            {
+                videoAndLinks.FileData = FileData.INV;
+            }
+            else
+            {
+                videoAndLinks.FileData = FileData.DEAD;
             }
         }
+    }
+
+    static async Task<bool> X(string assetUrl, string pdfFolderPath)
+    {
+        string downloadUrl = await FetchDownloadLink(assetUrl);
+        if (string.IsNullOrEmpty(downloadUrl)) return false;
+
+        // download zip file
+        string zipPath = Path.Combine(pdfFolderPath, assetUrl[(assetUrl.LastIndexOf('/') + 1)..]);
+        Console.WriteLine($"Downloading {assetUrl} to {zipPath}");
+        using HttpClient client = new();
+
+        try
+        {
+            byte[] bytes = await client.GetByteArrayAsync(downloadUrl);
+            await File.WriteAllBytesAsync(zipPath, bytes);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return false;
+        }
+
+        return true;
     }
 
     static async Task<string> FetchDownloadLink(string url)
